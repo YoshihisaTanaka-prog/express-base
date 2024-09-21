8d224e56f84fac10c8b3a62932261f851aae9eb5
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-09-22 05:37:29 +0900

    Basic moduleだけ別gitで管理しpullだけするよう更新

--- basic-modules/load-setting.js
+++ basic-modules/load-setting.js
@@ -1,23 +1,68 @@
 "use strict";
 
+const { readFileSync, existsSync, writeFile, lstatSync } = require("fs");
+
 module.exports = function (fileData){
   class Setting {
     constructor(path="", data){
       this.path = Object.freeze(path);
-      this.data = data;
+      if(data){
+        this.data = data;
+      } else {
+        this.data = {};
+        this.set({});
+      }
+      const getType = function (object) {
+        return Object.prototype.toString.call(object).split(" ")[1].slice(0, -1).toLowerCase();
+      }
+      Object.defineProperty(this, 'setUnit', {
+        value: Object.freeze(function(oldObject, newObject, self){
+          if(getType(oldObject) == getType(newObject) || ["null", "undefined"].includes(getType(oldObject))){
+            switch (getType(newObject)) {
+              case "object":
+                if(oldObject == null){
+                  oldObject = {};
+                }
+                for(const key in (newObject)){
+                  if(oldObject[key]){
+                    self.setUnit(oldObject[key], newObject[key], self);
+                  } else {
+                    oldObject[key] = newObject[key];
+                  }
+                }
+                return;
+              case "function":
+                return ;
+              default:
+                oldObject = newObject;
+                return ;
+            }
+          }
+          console.error("type error while updating to\n" + JSON.stringify(newObject, null, 2));
+        }),
+        writable: false,
+        enumerable: false
+      });
+    }
+    set(newData){
+      this.setUnit(this.data, newData);
+    }
+    sortKeys(newData){
+      let currentObject;
     }
     static new(path="", isMustData){
-      try {
-        const loadedText = readFileSync(path, "utf-8");
-        const loadedData = loadedText == "" ? null : JSON.parse(loadedText);
-        return Object.freeze(new Setting(path, loadedData));
-      } catch (e) {
-        console.error(e);
-        if(isMustData){
-          console.error("stop this process because essential json file does not exist.");
+      if(existsSync(path)){
+        if(lstatSync(path).isFile()){
+          const loadedText = readFileSync(path, "utf-8");
+          const loadedData = loadedText == "" ? null : JSON.parse(loadedText);
+          return Object.freeze(new Setting(path, loadedData));
         }
-        return null;
       }
+      if(isMustData){
+        console.error("stop this process because essential json file does not exist.");
+        process.exit(1);
+      }
+      return Object.freeze(new Setting(path, {}));
     }
   }
   
--- basic-modules/setting.js
+++ basic-modules/setting.js
@@ -2,16 +2,25 @@
 
 const loadSetting = require("./load-setting");
 
+/*
+  ・前準備
+    ・アプリごとにこのファイルを設定する。
+    ・.gitignoreにこのファイルと.gitignoreを追加する。
+*/
+
+/*
 const settingFileInfo = [
   {
     "key": "app",
-    "path": "C:/Users/tany1/project/GasSupporter/app/settings/app-data.json",
-    "isMust": true
+    "path": "full_path_to_setting_json_file1",
+    "isMust": true  // if setting file set by the array element is essential, add '"isMust": true'
   },
   {
     "key": "user",
-    "path": "C:/Users/tany1/project/GasSupporter/app/settings/user-data.json"
+    "path": "full_path_to_setting_json_file2"
   }
 ];
+
+*/
 
 module.exports = loadSetting(settingFileInfo);
